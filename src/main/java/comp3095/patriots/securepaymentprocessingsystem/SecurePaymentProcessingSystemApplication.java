f438b4ae2660aeaa3ec5266c7437ad582fefa46d
munazchat <munazumrauf@gmail.com>
2020-11-05 11:06:58 -0500

    Project setup

---
+++ src/main/java/comp3095/patriots/securepaymentprocessingsystem/SecurePaymentProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package comp3095.patriots.securepaymentprocessingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePaymentProcessingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurePaymentProcessingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/comp3095/patriots/securepaymentprocessingsystem/SecurePaymentProcessingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package comp3095.patriots.securepaymentprocessingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurePaymentProcessingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
