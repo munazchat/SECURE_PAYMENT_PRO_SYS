4e6a85e05e0d1a93e357907f23d2fe7f2b76cfb1
Berantzino <36447407+Berantzino@users.noreply.github.com>
2020-11-08 17:14:22 +0100

    adds login functionality with Spring Security (#2)

--- src/main/java/comp3095/patriots/securepaymentprocessingsystem/bootstrap/BootStrapData.java
+++ src/main/java/comp3095/patriots/securepaymentprocessingsystem/bootstrap/BootStrapData.java
@@ -4,7 +4,9 @@ import comp3095.patriots.securepaymentprocessingsystem.domain.Role;
 import comp3095.patriots.securepaymentprocessingsystem.domain.User;
 import comp3095.patriots.securepaymentprocessingsystem.repository.RoleRepository;
 import comp3095.patriots.securepaymentprocessingsystem.repository.UserRepository;
+import comp3095.patriots.securepaymentprocessingsystem.service.UserService;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,24 +14,31 @@ public class BootStrapData implements CommandLineRunner {
 
 	private final UserRepository userRepository;
 	private final RoleRepository roleRepository;
+	private final UserService userService;
+	private final BCryptPasswordEncoder passwordEncoder;
 
-	public BootStrapData(UserRepository userRepository, RoleRepository roleRepository) {
+
+	public BootStrapData(UserRepository userRepository, RoleRepository roleRepository, UserService userService, BCryptPasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
 		this.roleRepository = roleRepository;
+		this.userService = userService;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 
+		roleRepository.save(new Role("CLIENT"));
+
 		User admin = new User(
 				"Lasse", "Berantzino", "Jægerstræde 8",
-				"lasse@berantzino.dk", "password"
+				"lasse@berantzino.dk", passwordEncoder.encode("password")
 				//"lasseken.berantzino@georgebrown.ca"
 		);
 		admin.getRoles().add(new Role("ADMIN"));
-
 		userRepository.save(admin);
-		roleRepository.save(new Role("CLIENT"));
+
+		//userService.saveAdmin(admin);
 
 		System.out.println("Started in bootstrap...");
 		System.out.println("Number of Users: " + userRepository.count());
---
+++ src/main/java/comp3095/patriots/securepaymentprocessingsystem/controller/LoginController.java
@@ -0,0 +1,14 @@
+package comp3095.patriots.securepaymentprocessingsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+}
